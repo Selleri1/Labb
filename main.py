58a1a4abf3d4fc6ba12960691062d7cccad9df16
Jonathan Niklasson Godar <jonathan.godar@hotmail.com>
2024-09-30 17:44:05 +0200

    Miniräknare grund

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hej")
+def rakn(t1, t2, op):
+    if op == '+':
+        return t1 + t2
+
+
+t1 = int(input('tal 1'))
+op = input('operator')
+t2 = int(input('tal 1'))
+
+
+print(rakn(t1, t2, op))
